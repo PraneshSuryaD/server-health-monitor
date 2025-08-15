e4d6fdeb2f30ba611786bcbb2b9e3df09ba79d44
ashvanth12k <2006ashvanth@gmail.com>
2025-08-16 00:33:33 +0530

    update1

--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import psutil
+import platform
+
 
 app = FastAPI()
 
@@ -14,7 +16,10 @@ app.add_middleware(
 
 @app.get("/status")
 def get_status():
+    osi_type = platform.system()
+    osi_version=platform.version()
+    architecture=platform.machine()
     cpu = psutil.cpu_percent(interval=1)
     memory = psutil.virtual_memory().percent
     disk = psutil.disk_usage('/').percent
-    return {"cpu": cpu, "memory": memory, "disk": disk}
+    return {"cpu": cpu, "memory": memory, "disk": disk,"os_type": osi_type, "os_version": osi_version, "architecture":architecture}
